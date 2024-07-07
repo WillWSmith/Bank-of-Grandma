a7143677f2c87165320d849419bd306459fda0a6
WillWSmith <will.w.smith94@gmail.com>
2024-07-07 18:25:30 -0400

    Setup LedgerTable component

---
+++ src/components/LedgerTable.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const LedgerTable = ({ ledger }) => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Date</th>
+          <th>Item</th>
+          <th>Debit</th>
+          <th>Credit</th>
+          <th>Balance</th>
+        </tr>
+      </thead>
+      <tbody>
+        {ledger.map(entry => (
+          <tr key={entry.id}>
+            <td>{entry.date}</td>
+            <td>{entry.item}</td>
+            <td>{entry.debit}</td>
+            <td>{entry.credit}</td>
+            <td>{entry.balance}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default LedgerTable;
